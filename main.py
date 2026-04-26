2d23aaa4d0e6134878a8928a8466a267e91c04d3
danitheswagger <gupta.danica1@gmail.com>
2026-04-26 14:31:06 +0530

    revising and adding comments till a star function

--- main.py
+++ main.py
@@ -1,96 +1,110 @@
-import numpy as np
-import cv2
-import heapq
-import time 
+import numpy as np#numpy for array operation on frames and grid 
+import cv2#for camera capture, image processing and display
+import heapq#for priority queue used in A*
+import time #for tracking replan intervals and dwell timer
+
+
 aruco=cv2.aruco
 aruco_dict=aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 parameters=aruco.DetectorParameters()
-webcam=cv2.VideoCapture(1)#connect to camera
+#ArUco setup-DICT_4X4_50 means markers are 4x4 black/white squares
+#marker 0= agent position, marker 1= goal position
+
+webcam=cv2.VideoCapture(1)#connect to phone camera via Droid camera (index 1)
 if not webcam.isOpened():
     exit()#guard if camera disconnects
-for i in range (50):
-   webcam.read()
+for i in range (50):#discard first 50 frames so camera sensor can warm up 
+   webcam.read()#first 50 frames are often too dark or blurry and would corrupt the background model
 
 CELL_SIZE=20 #Chose 20 as its accurate enough to detect obstacles well, and small enough that A* runs fast 
-ret,background=webcam.read() 
+ret,background=webcam.read() #capture one clean frame of the empty scene as background
 background=cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
-background=cv2.GaussianBlur(background,(5,5),0)
+background=cv2.GaussianBlur(background,(5,5),0)#blur to reduct noise before storing a s reference 
 kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+start = None #None until the user clicks- prevents A* running before a point is selected 
+goal = None#None until a user selects a goal point - provides A* with a point to go till from start
 
-start = None
-goal = None
+dwell_start=None #None as the agent hasn't entered arrival zone yet hence timer has not started 
+arrived= False# is a flag- starts as false because the agent hasn't arrived anywhere yet
 
-dwell_start=None
-arrived= False
+def mouse_callback(event, x, y, flags, param):#defining a mouse click funtion which will specify start and goal 
+    global start, goal#allows this funtion to modify the global start anf goal variables 
 
-def mouse_callback(event, x, y, flags, param):
-    global start, goal
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        r = y // CELL_SIZE
-        c = x // CELL_SIZE
+    if event==cv2.EVENT_LBUTTONDOWN:
+        r=y//CELL_SIZE#convert pixel y coordinates into grid row 
+        c=x//CELL_SIZE#convert pixel x coordinate to grid column 
 
         if start is None:
-            start = (r, c)
+            start = (r, c)#if start has not been defined then on clicking left define a start by recording row and column
             print("Start:", start)
 
         elif goal is None:
             goal = (r, c)
-            print("Goal:", goal)
+            print("Goal:", goal)#if goal has not been defined then on clicking left define a goal by recording a row and column 
 
         else:
-            # reset on third click
-            start = (r, c)
-            goal = None
-            print("Reset Start:", start)
+            # third click resets- new start point, clears goal so user can pick again
+            start=(r,c)
+            goal=None
+            print("Start:", start)
 
 
 
-def astar(grid,start,goal):
-    h=abs(start[0]-goal[0])+abs(start[1]-goal[1])
-    open_list=[]
-    heapq.heappush(open_list,(0+h,start[0],start[1]))
-    closed_set=set()
-    g_score={start:0}
-    parent={}
-    while open_list:
-        f,row,col=heapq.heappop(open_list)
-        current=(row,col)
+def astar(grid,start,goal):#define the A* algortihm through funtion
+    h=abs(start[0]-goal[0])+abs(start[1]-goal[1])#from the start and goal obtained, calculate the distance using manhattan distance 
+    open_list=[]#heap of cell to explore next, ordered by f score (lowest first)
+    heapq.heappush(open_list,(0+h,start[0],start[1]))#heapq.heappush() adds an item to the open_list defined with values being the f (g+h where g is initially zero) and h is the heuristic and start's x coordinate and start's y coordinate
+    closed_set=set()#cells already fully explored- skip if encountered again to prevent loops  
+    g_score={start:0}#defined a g_score dictionary as it will be used again and again for calculating f values 
+    parent={}#defined a parent dictionary as it will be used for trakcing the algorithm from goal to start 
+    while open_list:#keep exploring until no cells are left and goal is found
+        f,row,col=heapq.heappop(open_list)#pop cell with most lowest f score - most promising cell
+        current=(row,col)#treat as current position 
 
         if current==goal:
+            #goal reached- trace back through parent dict to reconstruct path
+            #parent[cell]= the cell we came from to reach that cell
             path=[]
             while current!=start:
                 path.append(current)
-                current=parent[current]
-            path.append(start)
-            path.reverse()
+                current=parent[current]#move backwards pne step
+            path.append(start)#add start as loop stops before it 
+            path.reverse()#flip from goal-> start to start-> goal
             return path
         
         if current in closed_set:
+            #skip if already explored - same cell can be pushed 
+            #to heap multiple times if a cheaper path is found later 
             continue 
-        closed_set.add(current)
+        closed_set.add(current)#marked as fully explored before checking neighbours 
 
-        directions=[(-1,0),(1,0),(0,-1),(0,1)]
+        directions=[(-1,0),(1,0),(0,-1),(0,1)]#up,down,left,right directions which can be travelled in 
         for dr,dc in directions:
             nr=row+dr
             nc=col+dc
             neighbour=(nr,nc)
 
             if nr<0 or nr>=grid.shape[0] or nc<0 or nc>=grid.shape[1]:
-                continue 
+                continue #skip if outside grid boundaries 
             if grid[nr][nc]==1:
-                continue
+                continue#skip is cell is blocked by obstacle 
             if neighbour in closed_set:
-                continue 
+                continue #skip if already explored 
+
+            #cost to reach nighbour via current cell 
+            #each step costs 1 for cardinal movement 
             new_g=g_score[current]+1
 
             if new_g<g_score.get(neighbour,float('inf')):
+                #found a cheaper path to this neighbour- update everything 
+                #float('inf) is default for unvisited cells- any real cost beats it 
                 g_score[neighbour]=new_g
-                h=abs(nr-goal[0])+abs(nc-goal[1])
-                f=new_g+h
-                heapq.heappush(open_list,(f,nr,nc))
-                parent[neighbour]=current
-    return None 
+                h=abs(nr-goal[0])+abs(nc-goal[1])#manhattan distance to goal 
+                f=new_g+h#total score=actual +estimate
+                heapq.heappush(open_list,(f,nr,nc))#add to heap with new score 
+                parent[neighbour]=current#record how we got here 
+    return None #open list exhausted- no path exists between start and goal 
+
 last_replan=time.time()
 path=None
  
